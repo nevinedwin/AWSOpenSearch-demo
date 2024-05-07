48679f8f9a13dddb286db8b31173d161c15ef03b
nevin.e <nevin.e@inapp.com>
2024-05-07 23:53:47 +0530

    test

--- dynamoStream/dynamoStream.mjs
+++ dynamoStream/dynamoStream.mjs
@@ -1,6 +1,6 @@
 export const main = async (event) => {
 
-    console.log(`Recieved Event: ${event}`);
+    console.log(`Recieved Event: ${JSON.stringify(event)}`);
     return {
         statusCode: 200,
         body: JSON.stringify(event)
